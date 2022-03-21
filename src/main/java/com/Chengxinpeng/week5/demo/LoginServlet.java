310fc6a53c548ea6b638a424da3ff80e57c73146
2002cxpcxp <2834283813@qq.com>
2022-03-21 20:19:41 +0800

    week5.1 commit

---
+++ src/main/java/com/Chengxinpeng/week5/demo/LoginServlet.java
@@ -0,0 +1,23 @@
+package com.Chengxinpeng.week5.demo;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    public void init () throws ServletException {
+        super.init();
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
